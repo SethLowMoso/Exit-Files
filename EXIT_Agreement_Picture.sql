444f8a4b2b39eaaffd83d4412540fe7b8b5719d3
Seth Low <Seth.Low@MOTIONSOFT.COM>
2016-05-03 11:26:23 -0400

    Adjusted to correct for the directory paths so agreements could be addressed.

--- EXIT_Agreement_Picture.sql
+++ EXIT_Agreement_Picture.sql
@@ -2,6 +2,8 @@ select  CONCAT('Copy F:\MosoMrmAssetStorage\abb15b02-c131-4bc0-8c5f-a3950a565770
 from  rpt_MemberList ml
 WHERE ml.BusinessUnitID = 205
 
+
+
 SELECT CONCAT('Copy F:\MosoMrmAssetStorage\abb15b02-c131-4bc0-8c5f-a3950a565770\DocManagementStore\'
 				,partyid
 				,'\2\'
@@ -10,7 +12,7 @@ SELECT CONCAT('Copy F:\MosoMrmAssetStorage\abb15b02-c131-4bc0-8c5f-a3950a565770\
 				,' - '
 				, a.Name
 				, ' - '
-				, CONVERT(DATE, ma.StartDate, 101) 
+				, CONVERT(VARCHAR(10), ma.StartDate, 110) 
 				,'"\'
 				,ma.MemberAgreementID
 				,'.pdf'
@@ -24,5 +26,41 @@ from  rpt_MemberList ml
 INNER JOIN dbo.MemberAgreement ma ON ml.PartyRoleID = ma.PartyRoleId
 INNER JOIN dbo.Agreement a ON a.AgreementID = ma.AgreementID
 WHERE ml.BusinessUnitID = 205
-	--AND MemberAgreementID = 3791835
+	AND MemberAgreementID = 3417632
 
+
+SELECT  CONVERT(VARCHAR(10), ma.StartDate, 110)
+FROM memberAgreement  ma
+WHERE memberAgreementId = 3417632
+
+
+--SELECT CONCAT('Copy "F:\MosoMrmAssetStorage\abb15b02-c131-4bc0-8c5f-a3950a565770\Agreements\'
+--		, ma.MemberAgreementID
+--		, '.pdf'
+--		, '   '
+--		,'D:\Exit\Schraffts\Agreements\'
+--		,PartyID
+--		,'_'
+--		,ma.MemberAgreementID
+--		,'.pdf'
+--		)
+--from  rpt_MemberList ml
+--INNER JOIN dbo.MemberAgreement ma ON ml.PartyRoleID = ma.PartyRoleId
+--INNER JOIN dbo.Agreement a ON a.AgreementID = ma.AgreementID
+--WHERE ml.BusinessUnitID = 205
+
+
+F:\MosoMrmAssetStorage\abb15b02-c131-4bc0-8c5f-a3950a565770\DocManagementStore\1049296\2\"#3347422 - Passport Corporate Conversion - 02-05-2016"\3347422.pdf.
+F:\MosoMrmAssetStorage\abb15b02-c131-4bc0-8c5f-a3950a565770\DocManagementStore\1049311\2\"#3347423 - Core Corporate Conversion - 02-05-2016"\3347423.pdf
+F:\MosoMrmAssetStorage\abb15b02-c131-4bc0-8c5f-a3950a565770\DocManagementStore\1049398\2\"#3347424 - Core Corporate Conversion - 02-05-2016"\3347424.pdf
+F:\MosoMrmAssetStorage\abb15b02-c131-4bc0-8c5f-a3950a565770\DocManagementStore\1049483\2\"#3347426 - Core Corporate Conversion - 02-05-2016"\3347426.pdf
+
+F:\MosoMrmAssetStorage\abb15b02-c131-4bc0-8c5f-a3950a565770\DocManagementStore\1858629\2\"#3417632 - Managed Club Schraffts - 02-05-2016"\3417632.pdf
+
+
+Copy "F:\MosoMrmAssetStorage\abb15b02-c131-4bc0-8c5f-a3950a565770\DocManagementStore\1858629\2\"#3417632 - Managed Club Schraffts - 08-07-2015"\3417632.pdf
+
+
+F:\MosoMrmAssetStorage\abb15b02-c131-4bc0-8c5f-a3950a565770\DocManagementStore\1858629\2\"#3417632 - Managed Club Schraffts - 07-08-2015"\3417632.pdf
+
+Copy "F:\MosoMrmAssetStorage\abb15b02-c131-4bc0-8c5f-a3950a565770\DocManagementStore\1858629\2\"#3417632 - Managed Club Schraffts - 07-08-2015"\3417632.pdf"        D:\Exit\Schraffts\Agreements\8257488_3417632.pdf
